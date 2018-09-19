59202c00bbe48940d368aa82fd64802e57d1ec57
wuhengqiang <wuhq5@asiainfo.com>
2018-09-19 14:49:46 +0800

    访问控制系统版本管理

---
+++ src/main/resources/static/admin/js/login.js
@@ -0,0 +1,32 @@
+
+layui.use(['form','layer'],function(){
+    var form = layui.form,
+        layer = parent.layer === undefined ? layui.layer : parent.layer;
+    $ = layui.jquery;
+
+    //登录按钮事件
+    form.on("submit(login)", function(data){
+        //等待加载动画
+        var loadIndex = layer.load(2, {shade: [0.3, '#333']});
+        if($('form').find('input[type="checkbox"]')[0].checked){
+            data.field.rememberMe = true;
+        }else{
+            data.field.rememberMe = false;
+        }
+        $.post(data.form.action, data.field, function(res) {
+            //关闭加载动画
+            layer.close(loadIndex);
+            if(res.success){
+                location.href="/"+res.url;
+            }else{
+                layer.msg(res.message);
+                $("#randImage").click();
+            }
+        });
+        return false;
+    });
+
+    $("#randImage").click(function () {
+        this.src = "/getCaptcha?t=" + Math.random();
+    });
+});
